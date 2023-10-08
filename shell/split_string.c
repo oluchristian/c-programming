641464e0a80895df4343c4a286a5433f62350672
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-10-08 01:11:11 +0100

    tokenization

--- shell/split_string.c
+++ shell/split_string.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    
+    int i = 1;
+    char *token;
+
+    for (i = 1; i < argc; i++)
+    {
+        token = strtok(argv[i], " ");
+        while (token != NULL)
+        {
+            printf("Token: %s\n", token);
+            token = strtok(NULL, " ");
+        }
+    }
+    return 0;
 }
