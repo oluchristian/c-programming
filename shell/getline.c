6dfc4b4671351fe0766fcb3cc350e936af9dd835
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-10-11 19:39:58 +0100

    task 1 fixed except ctrl + d issue

--- shell/getline.c
+++ shell/getline.c
@@ -1,22 +1,31 @@
-#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
 int main (int argc, char*argv[])
 {
     char *buffer = NULL, *prompt = "$ ";
+    int status;
     size_t n;
     ssize_t read;
     bool frm_pipe = false;
     pid_t newProcess;
 while (1 && !frm_pipe)
 {   
+  /**
+    * Print a prompt and getline inputs
+    */
+    write(STDOUT_FILENO, prompt, 2);
     /**Checks if command is from terminal or from program*/ 
-    if (isatty(STDIN_FILENO) == 0)
-        frm_pipe = true
+    /**if (isatty(STDIN_FILENO) == 0)
+        frm_pipe = true;*/
     /*Reads data from standard input - stdin*/
     read = getline(&buffer, &n, stdin);
     if (read == -1)
     {
-        perror("Error getline");
+        write(STDOUT_FILENO, "Error getline", 13);
         free(buffer);
     }
     /*replace the new line with null terminator*/
@@ -28,18 +37,23 @@ while (1 && !frm_pipe)
     newProcess = fork();
     if (newProcess == 0)
     {
-        execve(buffer, );
+        if (execve(buffer, argv, NULL) == -1)
+        {
+            write(STDOUT_FILENO, "Error (execve)", 14);
+            exit(EXIT_FAILURE);
+        }
     }
     else if (newProcess == -1)
     {
-        perror("Error (fork)");
+        write(STDOUT_FILENO, "Error (fork)", 12);
         exit(EXIT_FAILURE);
     }
     else
     {
-        perror("Error (wait)");
-        exit(EXIT_FAILURE);
+        wait(&status);
     }
     
 }
+free(buffer);
+return (0);
 }
