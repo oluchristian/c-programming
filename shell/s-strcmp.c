bd74f7415c9ddfcadc0c590908a736dd889d09c0
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-10-12 22:44:22 +0100

    strcmp function

--- shell/s-strcmp.c
+++ shell/s-strcmp.c
@@ -2,6 +2,17 @@
 
 int strcmp(const char *s1, const char *s2)
 {
-    while (s1 != '\0' &&)
+    int count = 0;
+    while (*s1 != '\0' && *s2 != '\0')
+    {
+        if (*s1 != *s2 )
+        {
+            return (*s1 - *s2);
+        }
+        s1++;
+        s2++;
+        count++;
+    }
+    return (*s1 - *s2);
 
 }
