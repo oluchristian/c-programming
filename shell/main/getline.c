455d916cef5c8703232d1b46af0c85b2c35c477e
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-10-17 17:33:01 +0100

    added the exit feature

--- shell/main/getline.c
+++ shell/main/getline.c
@@ -1,5 +1,5 @@
 #include "shell.h"
-int main (int argc, char*argv[])
+int main(int argc, char *argv[])
 {
     char *buffer = NULL, *prompt = "$ ";
     char *token, *command, *full_path, *exit_token;
@@ -9,65 +9,72 @@ int main (int argc, char*argv[])
     ssize_t read;
     bool frm_pipe = false;
     pid_t newProcess;
-while (1 && !frm_pipe)
-{   
-    int argCount = 0;
-  /**
-    * Print a prompt and getline inputs
-    */
-    write(STDOUT_FILENO, prompt, 2);
-    /**Checks if command is from terminal or from program*/ 
-    /**if (isatty(STDIN_FILENO) == 0)
-        frm_pipe = true;*/
-    /*Reads data from standard input - stdin*/
-    read = getline(&buffer, &n, stdin);
-    if (read == -1)
+    while (1 && !frm_pipe)
     {
-        write(STDOUT_FILENO, "Error getline", 13);
-        write(STDOUT_FILENO, "\n", 2);
-        exit(EXIT_FAILURE);
-    }
-
-    /*replace the new line with null terminator*/
-    if (buffer[read - 1] == '\n')
-        buffer[read - 1] = '\0';
-
-    /**Tokenize*/
-    token = strtok(buffer, " ");
-
-    /**Save commands to an array*/
-
-    while (token != NULL)
-    {
-        arguements[argCount] = token;
-        argCount++;
-        token = strtok(NULL, " ");
-    }
-
-    arguements[argCount] = NULL;
-    /**Create a fork */
-    newProcess = fork();
-    if (newProcess == 0)
-    {
-        /*Full path of the command*/
-        full_path = get_path(arguements[0]);
-        if (execve(full_path, arguements, NULL) == -1)
+        int argCount = 0;
+        /**
+         * Print a prompt and getline inputs
+         */
+        write(STDOUT_FILENO, prompt, 2);
+        /**Checks if command is from terminal or from program*/
+        /**if (isatty(STDIN_FILENO) == 0)
+            frm_pipe = true;*/
+        /*Reads data from standard input - stdin*/
+        read = getline(&buffer, &n, stdin);
+        if (read == -1)
         {
-            perror("error");
+            write(STDOUT_FILENO, "Error getline", 13);
+            write(STDOUT_FILENO, "\n", 2);
             exit(EXIT_FAILURE);
         }
+
+        /*replace the new line with null terminator*/
+        if (buffer[read - 1] == '\n')
+            buffer[read - 1] = '\0';
+
+        /**Tokenize*/
+        token = strtok(buffer, " ");
+
+        /**Save commands to an array*/
+
+        while (token != NULL)
+        {
+            arguements[argCount] = token;
+            argCount++;
+            token = strtok(NULL, " ");
+        }
+
+        arguements[argCount] = NULL;
+
+        /**Exit the shell if user enters the exit command*/
+        if (strcmp(arguements[0], "exit") == 0)
+        {
+            free(buffer);
+            exit(0);
+        }
+        /**Create a fork */
+        newProcess = fork();
+        if (newProcess == 0)
+        {
+            /*Full path of the command*/
+            full_path = get_path(arguements[0]);
+            if (execve(full_path, arguements, NULL) == -1)
+            {
+                perror("error");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (newProcess == -1)
+        {
+            write(STDOUT_FILENO, "Error (fork)", 12);
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(&status);
+        }
+        argCount = 0;
     }
-    else if (newProcess == -1)
-    {
-        write(STDOUT_FILENO, "Error (fork)", 12);
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        wait(&status);
-    }
-    argCount = 0;
-}
-free(buffer);
-return (0);
+    free(buffer);
+    return (0);
 }
